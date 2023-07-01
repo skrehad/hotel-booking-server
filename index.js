bd1299a87363e1bf5459c4459d54137ec6270d5b
rehadhasan <rehadhasan664@gmail.com>
2023-07-02 01:40:21 +0600

    added mongodb in backend server

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 // var jwt = require("jsonwebtoken");
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bwvhp.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", async (req, res) => {
   res.send("doctor portal running");
