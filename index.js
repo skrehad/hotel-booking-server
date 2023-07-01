61bd7452b684f1c0da0db6b0fa8120e17e9bda3e
rehadhasan <rehadhasan664@gmail.com>
2023-07-02 02:02:06 +0600

    added blog collection in backend server

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bwvhp.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -19,13 +20,28 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
+    const blogCollection = client
+      .db("hotel-booking")
+      .collection("blogCollection");
+
+    app.get("/blog", async (req, res) => {
+      const query = {};
+      const users = await blogCollection.find(query).toArray();
+      res.send(users);
+    });
+
+    app.post("/blog", async (req, res) => {
+      const user = req.body;
+      const result = await blogCollection.insertOne(user);
+      res.send(result);
+    });
   } finally {
   }
 }
 run().catch(console.dir);
 
 app.get("/", async (req, res) => {
-  res.send("doctor portal running");
+  res.send("hotel booking running");
 });
 
-app.listen(port, () => console.log(`doctor portal running on ${port}`));
+app.listen(port, () => console.log(`hotel booking running on ${port}`));
